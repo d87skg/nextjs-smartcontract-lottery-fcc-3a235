8a73bed6873b159b933bbd4d52057553d67bba35
d87skg <jmhbldg@gmail.com>
2025-11-24 21:38:03 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,35 @@
-import { MoralisProvider } from "react-moralis";
-import { NotificationProvider } from "web3uikit";
 import "../styles/globals.css";
+import "@web3modal/react/dist/css/reset.css";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { base } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
-function MyApp({ Component, pageProps }) {
+const { chains, provider } = configureChains(
+  [base],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => ({ http: "https://mainnet.base.org" }),
+    }),
+    publicProvider(),
+  ]
+);
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  provider,
+});
+
+const ethereumClient = new Web3Modal(
+  { projectId: "YOUR_PROJECT_ID" }, // 随便填，后面用不到
+  wagmiClient
+);
+
+export default function App({ Component, pageProps }) {
   return (
-    <MoralisProvider initializeOnMount={false}>
-      <NotificationProvider>
-        <Component {...pageProps} />
-      </NotificationProvider>
-    </MoralisProvider>
+    <WagmiConfig client={wagmiClient}>
+      <Component {...pageProps} />
+    </WagmiConfig>
   );
 }
-
-export default MyApp;
